17b4e5ed737d28d9b4d38b7106b30e3aff006cd8
Eldubo <magondubi@gmail.com>
2025-04-10 12:04:38 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Tp1-React-Dubin-Sarobe
+ 
---
+++ src/assets/componentes/Form.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Form (){
+    return (
+        <div>
+            <form action="">
+                <label htmlFor="">Nombre Mascota</label>
+                <input type="text" name="" id="" />
+                <label htmlFor="">Nombre Dueño</label>
+                <input type="text" name="" id="" />
+                <label htmlFor="">Fecha</label>
+                <input type="date" name="" id="" />
+                <label htmlFor="">Hora</label>
+                <input type="datetime" name="" id="" />
+                <label htmlFor="">Sintomas</label>
+                <input type="text" name="" id="" />
+                <button type="submit">Agregar Cita</button>
+            </form>
+        </div>
+    )
+}
+export default Form
